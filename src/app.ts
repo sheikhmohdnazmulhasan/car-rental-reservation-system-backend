5266357e4df56af54b47f2e2e21f506907b2080f
mdnazmul42726 <nazmulofficial@outlook.com>
2024-07-04 23:21:13 +0600

    not found route created and http-status npm package installed

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 
 const app: Application = express();
@@ -7,8 +7,19 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+// test endpoint
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
 });
+
+// not found route
+app.all('*', (req: Request, res: Response) => {
+    res.status(404).json({
+        success: false,
+        statusCode: 404,
+        message: "Not Found",
+    });
+    
+})
 
 export default app;
