4bdf7baf87a05c66728b08e227de58681eb41268
mdnazmul42726 <nazmulofficial@outlook.com>
2024-07-05 00:15:32 +0600

    zod model modified

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,11 +1,13 @@
-import { z } from 'zod';
+import { object, z } from 'zod';
 
 const createUserValidationSchema = z.object({
-    name: z.string().nonempty({ message: "Name is required" }),
-    email: z.string().email({ message: "Invalid email address" }),
-    role: z.enum(['admin', 'user'], { message: "Role must be either 'admin' or 'user'" }),
-    address: z.string().optional(),
-    password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
+    body: z.object({
+        name: z.string().nonempty({ message: "Name is required" }),
+        email: z.string().email({ message: "Invalid email address" }),
+        role: z.enum(['admin', 'user'], { message: "Role must be either 'admin' or 'user'" }),
+        address: z.string().optional(),
+        password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
+    })
 });
 
 export const UserValidation = { createUserValidationSchema };
