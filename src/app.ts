f1aa662b51758b10952ac6ba095306f6553f9c3f
mdnazmul42726 <nazmulofficial@outlook.com>
2024-07-04 23:16:29 +0600

    cors and express.json parser added on the app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express, { Application } from 'express';
+import cors from 'cors';
 
 const app: Application = express();
+
+// parser
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
