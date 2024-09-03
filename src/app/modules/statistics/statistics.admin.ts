d9430b15a65b2fe0e353b5ac5a2464f66e751b3b
mdnazmul42726 <nazmulofficial@outlook.com>
2024-09-04 01:47:52 +0600

    fix some buges

--- src/app/modules/statistics/statistics.admin.ts
+++ src/app/modules/statistics/statistics.admin.ts
@@ -5,7 +5,6 @@ import Car from "../car/car.model";
 import { TBooking } from "../booking/booking.interface";
 
 async function statistics(req: Request, res: Response, next: NextFunction) {
-    const session = await mongoose.startSession();
     const result: Record<string, unknown> = {};
 
     try {
@@ -13,11 +12,10 @@ async function statistics(req: Request, res: Response, next: NextFunction) {
         result.total_booking = await Booking.countDocuments();
         // total vehicles
         result.total_vehicles = await Car.countDocuments({ isDeleted: false });
-
         // total revenue
         result.total_revenue = (await Booking.find())
             .filter(booking => booking.totalCost > 0) // Filter out negative values
-            .reduce((acc, booking) => booking.totalCost + acc, 0);
+            .reduce((acc, booking) => booking.totalCost + acc, 0)
 
         if (!result.total_booking || !result.total_vehicles || !result.total_revenue) {
             res.status(400).json({
@@ -36,7 +34,6 @@ async function statistics(req: Request, res: Response, next: NextFunction) {
     } catch (error) {
         next(error);
     }
-    console.log(result);
 }
 
 export const adminStatistics = { statistics }
