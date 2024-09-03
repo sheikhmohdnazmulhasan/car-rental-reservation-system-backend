b14c4ec6219b76d16e1cf371b18925a3513cc0c8
mdnazmul42726 <nazmulofficial@outlook.com>
2024-09-04 00:36:30 +0600

    admin statistics fetched successfully

--- src/app/modules/statistics/statistics.admin.ts
+++ src/app/modules/statistics/statistics.admin.ts
@@ -1,5 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
+import Booking from "../booking/booking.model";
+import Car from "../car/car.model";
+import { TBooking } from "../booking/booking.interface";
 
 async function statistics(req: Request, res: Response, next: NextFunction) {
     const session = await mongoose.startSession();
@@ -7,11 +10,33 @@ async function statistics(req: Request, res: Response, next: NextFunction) {
 
     try {
         // Total Bookings
-        const total_booking = 
+        result.total_booking = await Booking.countDocuments();
+        // total vehicles
+        result.total_vehicles = await Car.countDocuments({ isDeleted: false });
+
+        // total revenue
+        result.total_revenue = (await Booking.find())
+            .filter(booking => booking.totalCost > 0) // Filter out negative values
+            .reduce((acc, booking) => booking.totalCost + acc, 0);
+
+        if (!result.total_booking || !result.total_vehicles || !result.total_revenue) {
+            res.status(400).json({
+                success: false,
+                message: 'admin statistics fetched Unsuccessful',
+                data: []
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: 'admin statistics fetched successfully',
+            data: result
+        });
+
     } catch (error) {
         next(error);
     }
-
+    console.log(result);
 }
 
 export const adminStatistics = { statistics }
